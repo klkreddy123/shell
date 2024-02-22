80bc75370c7e9e8a2dd73634d5b4cb69d02a67d9
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2024-02-22 13:41:23 +0530

    02-variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am fine $PERSON1"
+echo "$PERSON1: What are you doing $PERSON2?"
+echo "$PERSON2: I am travelling $PERSON1"
