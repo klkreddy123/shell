19b93119daacae6b0362b563cfdfb2c3f77aaed6
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2024-02-23 10:39:43 +0530

    functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,12 +1,16 @@
 VALIDATE(){
 if [ $1 -ne 0 ]
     then
-        echo "$2 is failure"
+        echo "$2 is $RED failure $NC"
         exit 1
     else
-        echo "$2 is success"
+        echo "$2 is $GREEN success $NC"
 fi
 }
+
+RED="\e[31m"
+GREEN="\e[32m"
+NC="\e[0m"
 USER=$(id -u)
 
 if [ $USER -ne 0 ]
