5e545ad018177425921fb0e4de3e23fd156a7cbe
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2024-02-24 12:34:05 +0530

    roboshell-catalogue

--- roboshell/catalogue.sh
+++ roboshell/catalogue.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 VALIDATE(){
-if [ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo "$2 is failure"
+        echo -e "$2 application is $RED failure $NOCOLOR"
     else
-        echo "$2  is success"
+        echo -e "$2 application is $GREEN success $NOCOLOR"
 fi
+
 }
+
 
 DATE=$(date +%F)
 RED="\e[31m"
@@ -24,54 +26,58 @@ if [ $USER -ne 0 ]
 fi
 
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOG_FILE
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
 
 VALIDATE $? "Installing NodeJS repos"
 
-yum install nodejs -y &>> $LOG_FILE
+yum install nodejs -y &>>$LOG_FILE
 
 VALIDATE $? "Installing NodeJS" 
 
-useradd roboshop &>> $LOG_FILE
+useradd roboshop &>>$LOG_FILE
 
 VALIDATE $? "Adding user"
 
-mkdir /app &>> $LOG_FILE
+mkdir /app &>>$LOG_FILE
 
 VALIDATE $? "creating directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOG_FILE 
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOG_FILE 
 
 VALIDATE $? "downloading catalogue"
 
-cd /app &>> $LOG_FILE
+cd /app &>>$LOG_FILE
 
 VALIDATE $? "changing directory"
 
-npm install &>> $LOG_FILE
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+
+VALIDATE $? "unxipping catalogue"
+
+npm install &>>$LOG_FILE
 
 VALIDATE $? "npm installing"
 
-cp /root/shell/roboshell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOG_FILE
+cp /root/shell/roboshell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
 
 VALIDATE $? "copying catalogue service"
 
-systemctl daemon-reload &>> $LOG_FILE
+systemctl daemon-reload &>>$LOG_FILE
 
 VALIDATE $? "daemon reload"
 
-systemctl enable catalogue &>> $LOG_FILE
+systemctl enable catalogue &>>$LOG_FILE
 
 VALIDATE $? "enable catalogue"
 
-systemctl start catalogue &>> $LOG_FILE
+systemctl start catalogue &>>$LOG_FILE
 
 VALIDATE $? "start catalogue"
 
-cp /root/shell/roboshell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
+cp /root/shell/roboshell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
 VALIDATE $? "copy mongo repo"
 
-yum install mongodb-org-shell -y &>> $LOG_FILE
+yum install mongodb-org-shell -y &>>$LOG_FILE
 
 VALIDATE $? "installing mongodb"
